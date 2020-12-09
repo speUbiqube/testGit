5940ffddfd8dc70cb1847dbd0adaa7bd4ae8457b
jboss <jboss@c24260a04ce8>
2020-12-09 10:09:11 +0000

    Initial commit with existing files

---
+++ a10_thunder_axapi/do_get_sd_conf.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * A10 Thunder aXAPI
+ *
+ * SD conf
+ *
+ */
+
+require_once 'smsd/sms_common.php';
+
+require_once load_once('a10_thunder_axapi', 'a10_thunder_axapi_connect.php');
+require_once load_once('a10_thunder_axapi', 'a10_thunder_axapi_configuration.php');
+
+try
+{
+  $ret = a10_thunder_axapi_connect();
+  if ($ret !== SMS_OK)
+  {
+  	throw new SmsException("", ERR_SD_CONNREFUSED);
+  }
+
+  // Get the conf on the router
+  $conf = new a10_thunder_axapi_configuration($sdid);
+  $SMS_RETURN_BUF = $conf->get_running_conf();
+  a10_thunder_axapi_disconnect();
+}
+catch(Exception | Error $e)
+{
+  a10_thunder_axapi_disconnect();
+  return $e->getCode();
+}
+
+return SMS_OK;
+
+?>
