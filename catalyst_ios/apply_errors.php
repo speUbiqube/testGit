eb3bf6ef003fba4e8a54a14d0fd5ccbfd0e134b4
jboss <jboss@c24260a04ce8>
2020-12-09 10:01:28 +0000

    Initial commit with existing files

---
+++ catalyst_ios/apply_errors.php
@@ -0,0 +1,22 @@
+ <?php
+/*
+ * Version: $Id$
+ * Created: Jun 8, 2012
+ */
+
+// Error strings to detect failure in configuration application
+$apply_errors = array(
+  "@is in use@",
+  "@Cannot change configuration@",
+  "@Error opening flash@",
+  "@Invalid input detected@",
+  "@Incomplete command@",
+  "@Ambiguous command@",
+  "@Command rejected@",
+  "@Invalid@",
+  "@Error opening system@",
+  "@Speed autonegotiation subset must not contain 1Gbps when duplex is set to half@",
+  "@Error opening tftp@"
+);
+
+?>
