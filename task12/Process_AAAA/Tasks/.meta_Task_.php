b1f0a3bdc0d2f94bc1d99556c1180127c1c99989
jboss <jboss@fccab6324154>
2021-02-08 13:24:37 +0000

    Updating BPMN on  - 08-02-2021 13:24:37 - ncroot

--- task12/Process_AAAA/Tasks/.meta_Task_.php
+++ task12/Process_AAAA/Tasks/.meta_Task_.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1612790673490</value>
+            <value>1612790677461</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1612790673485</value>
+            <value>1612790677456</value>
         </entry>
         <entry>
             <key>TAG</key>
