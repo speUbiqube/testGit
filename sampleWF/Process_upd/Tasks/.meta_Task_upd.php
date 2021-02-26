9f3669a0aca772eb6f22ca355ab041c5ffa29536
jboss <jboss@f0bf35e24388>
2021-02-26 09:07:36 +0000

    Updating BPMN on  - 26-02-2021 09:07:36 - ncroot

--- sampleWF/Process_upd/Tasks/.meta_Task_upd.php
+++ sampleWF/Process_upd/Tasks/.meta_Task_upd.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1614330394386</value>
+            <value>1614330456906</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1614330394382</value>
+            <value>1614330456900</value>
         </entry>
         <entry>
             <key>TAG</key>
