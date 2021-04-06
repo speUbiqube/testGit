3a7228f5c4ed0adbd6efffdcaf5d26e6855ae7bc
jboss <jboss@aa03ac9cfc06>
2021-04-06 09:52:04 +0000

    Updating BPMN on  - 06-04-2021 09:52:04 - ncroot

--- toutou/Process_t1/Tasks/.meta_Task_t1.php
+++ toutou/Process_t1/Tasks/.meta_Task_t1.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1617702590075</value>
+            <value>1617702724571</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1617702590071</value>
+            <value>1617702724566</value>
         </entry>
         <entry>
             <key>TAG</key>
--- toutou/Process_t1/Tasks/Task_t1.php
+++ toutou/Process_t1/Tasks/Task_t1.php
@@ -19,8 +19,8 @@ function list_args()
    *
    * Add as many variables as needed
    */
-  create_var_def('var_name', 'String');
-  create_var_def('var_name2', 'Integer');
+  create_var_def($v1, 'String');
+  create_var_def($v2, 'Managed Entity');
 }
 
 /**
@@ -35,7 +35,8 @@ function list_args()
  * For ex. if (empty($context['var_name']) || (empty($context['var_name2']) && empty($context['var_name3']))) => FAIL [Don't proceed]
  * Such cases need to be handled as per the Task logic
  */
-check_mandatory_param('var_name');
+check_mandatory_param($v1);
+check_mandatory_param($v2);
 
 /**
  * $context => Service Context variable per Service Instance
@@ -46,7 +47,7 @@ check_mandatory_param('var_name');
  *
  * ENTER YOUR CODE HERE
  */
-$context['var_name2'] = $context['var_name2'] + 1;
+$context[$v1] = $context[$v2] + 1;
 
 /**
  * Format of the Task response :
