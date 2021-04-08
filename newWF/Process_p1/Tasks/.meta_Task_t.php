68f97c8cddfe2c9ea333421310bdd31f9d217ffd
jboss <jboss@bcbd6e657044>
2021-04-08 08:54:41 +0000

    Updating BPMN on  - 08-04-2021 08:54:41 - ncroot

--- newWF/Process_p1/Tasks/.meta_Task_t.php
+++ newWF/Process_p1/Tasks/.meta_Task_t.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1617871875195</value>
+            <value>1617872081472</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1617871875189</value>
+            <value>1617872081464</value>
         </entry>
         <entry>
             <key>TAG</key>
--- newWF/Process_p1/Tasks/Task_t.php
+++ newWF/Process_p1/Tasks/Task_t.php
@@ -21,6 +21,7 @@ function list_args()
    */
   create_var_def('var_name', 'String');
   create_var_def('var_name2', 'Integer');
+  create_var_def('device', 'Device');
 }
 
 /**
@@ -36,7 +37,7 @@ function list_args()
  * Such cases need to be handled as per the Task logic
  */
 check_mandatory_param('var_name');
-
+check_mandatory_param('device');
 /**
  * $context => Service Context variable per Service Instance
  * All the user-inputs of Tasks are automatically stored in $context
